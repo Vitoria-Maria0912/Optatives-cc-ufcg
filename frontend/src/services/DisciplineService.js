e2d4a656df8eee884b37d1a170a5ec70d01ef6a2
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-10-24 15:25:21 -0300

    wip: Creating Discipline Form, Service and changing App.js.

---
+++ frontend/src/services/DisciplineService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5432/disciplines';
+const API_URL_PROTECTED = 'http://localhost:5432/protected/disciplines';
+
+export const createDiscipline = async (data) => axios.post(API_URL_PROTECTED, data);
+export const deleteAllDisciplines = async () => axios.delete(API_URL_PROTECTED);
+export const deleteDiscipline = async (id) => axios.delete(`${API_URL_PROTECTED}/${id}`);
+export const patchDiscipline = async (id) => axios.patch(`${API_URL_PROTECTED}/${id}`);
+export const putDiscipline = async (id) => axios.put(`${API_URL_PROTECTED}/${id}`);
+export const getOneDisciplineByID = async (id) => axios.patch(`${API_URL}/getByID/${id}`);
+export const getOneDisciplineByName = async (name) => axios.patch(`${API_URL}/getByName/${name}`);
+export const getAllDisciplines = async () => axios.get(API_URL);
+
