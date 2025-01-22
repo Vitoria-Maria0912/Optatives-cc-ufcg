afc8e9eebcdaa0634ebdde5ad6c6530e5a76e26c
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2025-01-21 22:48:18 -0300

    feat: Add the layout on 'App.js' and the CSS style on 'style.css'.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,132 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { getAllDisciplines, createDiscipline, deleteDiscipline, getOneDisciplineByName, patchDiscipline, putDiscipline, deleteAllDisciplines, getOneDisciplineByAcronym } from './routes/DisciplineRoutes';
-import DisciplineCreateForm from './components/forms/DisciplineCreateForm';
-import DisciplineForm from './components/forms/DisciplineForm';
-import ShowOneDiscipline from './components/forms/ShowOneDiscipline';
-import UpdateDiscipline from './components/forms/UpdateDiscipline';
-import DisciplinePatchForm from './components/forms/DisciplinePatchForm';
-import CenterMain from './components/layout/CenterMain';
+import React, { useState } from 'react';
+import { UserOutlined, TeamOutlined, DesktopOutlined, PieChartOutlined, FileOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './index.css';
 
 const App = () => {
+
+  const { Header, Content, Footer, Sider } = Layout;
+  const { SubMenu } = Menu;
   
-  const [disciplines, setDisciplines] = useState([]);
-  const [showDiscipline, setShowDiscipline] = useState(null);
-  const [showPutDiscipline, setShowPutDiscipline] = useState(null); 
-  const [showPatchDiscipline, setShowPatchDiscipline] = useState(null); 
+  const [collapsed, setCollapsed] = useState(false);
 
-  useEffect(() => { fetchDisciplines();}, []);
-
-  const fetchDisciplines = async () => {
-    try {
-      const response = await getAllDisciplines();
-      setDisciplines(response.data.disciplines);
-
-    } catch (error) {}
-  };
-
-  const handleShowOne = async (name) => {
-    try {
-        let response = await getOneDisciplineByName(name.name);
-        setShowDiscipline(response.data.discipline);
-    } catch (error) {
-        try {
-            let response = await getOneDisciplineByAcronym(name.name);
-            setShowDiscipline(response.data.discipline);
-        } catch (error) { alert(error.response.data.message); }
-
-    } finally { fetchDisciplines(); }
-  };
-
-  const handleCreate = async (formData) => {
-    try {
-      const successMessage = (await createDiscipline(formData)).data.message;
-      alert(successMessage);
-      fetchDisciplines();
-
-    } catch (error) { alert(error.response.data.message); }
-  };
-
-  const handleDelete = async (name) => {
-    try {
-      const { id } = (await getOneDisciplineByName(name.name)).data.discipline;
-      const successMessage = (await deleteDiscipline(id)).data.message;
-      alert(successMessage);
-      fetchDisciplines();
-      
-    } catch (error) { alert(error.response.data.message); }
-  };
-
-  const handleDeleteAll = async () => {
-    try {
-      const successMessage = (await deleteAllDisciplines()).data.message;
-      alert(successMessage);
-      fetchDisciplines();
-      
-    } catch (error) { alert(error.response.data.message); }
-  };
-
-  const handlePatch = async (name, field, value) => {
-    try {
-      const response = (await getOneDisciplineByName(name.name));
-      const { id } = response.data.discipline;
-      setShowPatchDiscipline(response.data.discipline);
-      const successMessage = (await patchDiscipline(id, field, value)).data.message;
-      fetchDisciplines();
-      alert(successMessage);
-      
-    } catch (error) { alert(error.response.data.message); }
-  };
-
-  const handleUpdate = async (name, discipline) => {
-    try {
-      const response = (await getOneDisciplineByName(name.name));
-      const { id } = response.data.discipline;
-      setShowPutDiscipline(response.data.discipline);
-      const successMessage = (await putDiscipline(id, {discipline})).data.message;
-      alert(successMessage);
-      fetchDisciplines();
-      
-    } catch (error) { alert(error.response.data.message); }
+  const onCollapse = (collapsed) => {
+    console.log(collapsed);
+    setCollapsed(collapsed);
   };
 
   return (
-    <div>
-      <h1 style={{color: 'indigo', textAlign:'center'}}>Optatives CC_UFCG</h1>
-      <CenterMain/>
-
-      {/* <h2 style={{color: 'indigo', textAlign:'center'}}>Create discipline</h2>
-      <DisciplineCreateForm onSubmit={handleCreate} />
-
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Delete all disciplines</h2>
-      <button style={{cursor: 'pointer', alignItems:'center'}} onClickCapture={handleDeleteAll}>Delete all disciplines</button>
-      
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Delete discipline</h2>
-      <DisciplineForm onSubmit={handleDelete} submitText="Delete Discipline"/>
-
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Show one discipline</h2>
-      <DisciplineForm onSubmit={handleShowOne} submitText="Show Discipline"/>
-
-      {showDiscipline && <ShowOneDiscipline selectedDiscipline={showDiscipline} />}
-
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Update discipline field</h2>
-      <DisciplineForm onSubmit={handleShowOne} submitText="Show discipline to update"/>
-      <DisciplinePatchForm onSubmit={handlePatch} submitText="Update discipline field"/>
-
-      {showPatchDiscipline && <UpdateDiscipline selectedDiscipline={showPatchDiscipline} />}
-
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Update discipline</h2>
-      <DisciplineForm onSubmit={handleUpdate} submitText="Show discipline to update"/>
-
-      {showPutDiscipline && <UpdateDiscipline selectedDiscipline={showPutDiscipline} />}
-
-      <h2 style={{color: 'indigo', textAlign:'center'}}>Show disciplines</h2>
-      <ul>
-        {disciplines.map((discipline) => (
-          <li key={discipline.id}>{discipline.name} </li>
-        ))}
-      </ul> */}
-
-    </div>
+      <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" >
+              <span className="logo-T">T</span>
+              <span className="logo-text">rilharei</span>
+          </div>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+              </Menu.Item>
+              <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+              </Menu.Item>
+              <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="7">Team 2</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="8" icon={<FileOutlined />}>
+              File
+              </Menu.Item>
+          </Menu>
+          </Sider>
+          <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+      </Layout>
   );
 };
 
