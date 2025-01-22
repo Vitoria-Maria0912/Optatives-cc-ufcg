cca63d08258480590620eacd47a87ad634060ec9
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2025-01-22 19:13:16 -0300

    feat: Create redirecting on click.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { HomeOutlined, UserOutlined, BookOutlined, UsbOutlined, ReloadOutlined, ReadOutlined, SunOutlined, BulbOutlined } from '@ant-design/icons';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { HomeOutlined, UserOutlined, BookOutlined, UsbOutlined, BulbOutlined } from '@ant-design/icons';
+import { Menu, Layout } from 'antd';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import * as Main from './components/layout/CenterMain.js'
 import './style.css';
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
 
   const onCollapse = (collapsed) => { setCollapsed(collapsed); };
 
+  const navigate = useNavigate();
+  
   return (
       <Layout style={{ minHeight: '100vh' }}>
           <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
@@ -20,22 +24,23 @@ const App = () => {
               <span className="logo-text">rilharei</span>
           </div>
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="vertical">
-              <Menu.Item key="home" icon={<HomeOutlined />}>Home</Menu.Item>
-              <Menu.Item key="2" icon={<BulbOutlined />}>About</Menu.Item>
-              <SubMenu key="sub1" icon={<UserOutlined />} title="Usuário">
-                <Menu.Item key="11">Criar novo usuário</Menu.Item>
+              <Menu.Item key="home" icon={<HomeOutlined />} onClick={() => navigate('/')}>Home</Menu.Item>
+              <Menu.Item key="2" icon={<BulbOutlined />} onClick={() => navigate('/about')}>About</Menu.Item>
+
+              <SubMenu key="sub1" icon={<UserOutlined />} title="Usuário" onClick={() => navigate('/users')}>
+                <Menu.Item key="11" onClick={() => navigate('/create')}>Criar novo usuário</Menu.Item>
                 <Menu.Item key="12">Criar login</Menu.Item>
                 <Menu.Item key="13">Editar usuário</Menu.Item>
                 <Menu.Item key="14">Visualizar usuários</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" icon={<UsbOutlined />} title="Disciplina">
-                <Menu.Item key="21">Criar disciplina</Menu.Item>
+              <SubMenu key="sub2" icon={<UsbOutlined />} title="Disciplina" onClick={() => navigate('/disciplines')}>
+                <Menu.Item key="21">Criar nova disciplina</Menu.Item>
                 <Menu.Item key="22">Apagar disciplina</Menu.Item>
                 <Menu.Item key="23">Editar disciplina</Menu.Item>
-                <Menu.Item key="24">Visualizar todas asdisciplinas</Menu.Item>
+                <Menu.Item key="24">Visualizar todas as disciplinas</Menu.Item>
                 <Menu.Item key="25">Visualizar disciplina</Menu.Item>                
               </SubMenu>
-              <SubMenu key="sub3" icon={<BookOutlined />} title="Planejamento">
+              <SubMenu key="sub3" icon={<BookOutlined />} title="Planejamento" onClick={() => navigate('/planning')}>
                 <Menu.Item key="31">Criar planejamento</Menu.Item>
                 <Menu.Item key="32">Salvar planejamento</Menu.Item>
                 <Menu.Item key="33">Editar planejamento</Menu.Item>
@@ -46,11 +51,14 @@ const App = () => {
           <Layout>
             <Header style={{ background: '#fff', padding: 0 }} />
             <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+                <Routes>
+                    <Route path="/" exact element={<Main.Home/>} />
+                    <Route path="/about" exact element={<Main.About/>} />
+                    <Route path="/users" exact element={<Main.UserLinks/>} />
+                    <Route path="/auth/login" exact element={<Main.LoginLinks/>} />
+                    <Route path="/disciplines" exact element={<Main.DisciplineLinks/>} />
+                    <Route path="/planning" exact element={<Main.PlanningLinks/>} />
+                </Routes>
             </Content>
             <Footer style={{ textAlign: 'center' }}>Ant Design © 2018 Created by Ant UED</Footer>
           </Layout>
