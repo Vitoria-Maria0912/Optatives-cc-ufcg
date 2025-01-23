063f7b5b57e02dc68b22e282f753e628a11b865c
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-22 22:09:21 -0300

    style: adding planning and cards

--- frontend/src/components/layout/CenterMain.js
+++ frontend/src/components/layout/CenterMain.js
@@ -2,6 +2,8 @@ import React from "react";
 // import { Route, Routes, useNavigate } from "react-router-dom";
 import { Breadcrumb } from "antd";
 
+import "./style.css"
+
 export const Home = () => { 
     return (
         <div>
@@ -9,6 +11,56 @@ export const Home = () => {
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>Fluxograma</Breadcrumb.Item>
             </Breadcrumb>
+            <div className="planning">
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+                <div className="period">
+                    <div className="card">
+                        <h3 className="title">P1</h3>
+                    </div>
+                    <div className="card">
+                        <h3 className="title">LP1</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
