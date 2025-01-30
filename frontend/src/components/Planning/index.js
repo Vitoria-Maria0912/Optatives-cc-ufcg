8ea435ed6be33618748e5aeb7c2200d3baddcbed
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-30 09:23:43 -0300

    feat: adding a component to manage planning layout

---
+++ frontend/src/components/Planning/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import "./style.css"
+import Card from "../Card";
+import DropZone from "../DropZone";
+
+const Planning = ({}) => {
+    const [cards, setCards] = useState({
+        periodo1: ["P1", "LP1"],
+        periodo2: ["P2"],
+        periodo3: [],
+        periodo4: [],
+        periodo5: [],
+        periodo6: [],
+        periodo7: [],
+        periodo8: [],
+        periodo9: [],
+    })
+
+    return (
+        <div className="planning">
+            {Object.keys(cards).map(period => (
+                <div className="period">
+                    <DropZone targetPeriod={period} index={0} setCards={setCards}/>
+                    {cards[period].map((card, index) => (
+                        <React.Fragment>
+                            <Card card={card} period={period}/>
+                            <DropZone targetPeriod={period} index={index + 1} setCards={setCards}/>
+                        </React.Fragment>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+export default Planning;
+
